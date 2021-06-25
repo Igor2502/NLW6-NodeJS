b20b427081f534936acc781a295a6982f09cc3c5
Igor <igor.gds.00@gmail.com>
2021-06-25 19:15:52 -0300

    Terceiro dia

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import { router } from './routes';
 
 import './database';
@@ -8,5 +9,12 @@ const app = express();
 
 app.use(express.json());
 app.use(router);
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if(err instanceof Error) {
+    return res.status(400).json({ error: err.message});
+  }
+
+  return res.status(500).json({ status: 'error', message: 'Internal Server Error' });
+});
 
 app.listen(3000, () => console.log('Server is running!'));
